13d3a410543eb010f26e121791b647005aa6c2e4
MF-KIMPOEL <igelvolker93@gmail.com>
2026-05-18 07:45:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -426,8 +426,7 @@ with Dataset:
 # HALAMAN 4 — TENTANG SAYA
 # ══════════════════════════
 with About_Me:
-    st.markdown("<h1 style=\"font-family:'DM Serif Display',serif;font-size:3rem;color:#f0ede8;margin-bottom:2rem;\">Tentang Saya</h1>", unsafe_allow_html=True)
-
+st.markdown("<div style='border:1px solid #1e1e1e;border-radius:4px;overflow:hidden;'><img src='https://raw.githubusercontent.com/MF-KIMPOEL/projek_ML_Musik/main/hafidz.jpeg' style='width:100%;display:block;'></div>", unsafe_allow_html=True)
     cf, ci = st.columns([1,2], gap="large")
     with cf:
         st.markdown("<div style='border:1px solid #1e1e1e;border-radius:4px;overflow:hidden;'><img src='https://github.com/MF-KIMPOEL/projek_ML_Musik/blob/main/hafidz.jpeg' style='width:100%;display:block;'></div>", unsafe_allow_html=True)
